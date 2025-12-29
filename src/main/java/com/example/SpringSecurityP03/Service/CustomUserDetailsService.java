5338c51e2b1617c42a7ea195c07886bfa70447e0
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-29 16:58:35 +0530

    commit

---
+++ src/main/java/com/example/SpringSecurityP03/Service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.SpringSecurityP03.Service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        if(!username.equals("paras")){
+            throw  new UsernameNotFoundException("user not found");
+        }
+
+        String encodePass=new BCryptPasswordEncoder().encode("root");
+
+        return User.withUsername(username).password(encodePass).roles("USER").build();
+    }
+}
