e7556b95ae7abfaceda3d61457e2e2018dd42383
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-29 16:58:40 +0530

    commit

--- src/main/java/com/example/SpringSecurityP03/config/webConfig.java
+++ src/main/java/com/example/SpringSecurityP03/config/webConfig.java
@@ -1,4 +1,55 @@
 package com.example.SpringSecurityP03.config;
 
+
+import com.example.SpringSecurityP03.Service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.PublicKey;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class webConfig {
+
+
+    @Autowired
+     private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.authorizeHttpRequests(req->
+                req.requestMatchers("/security/aboutUs","/security/contactUs").permitAll().anyRequest().authenticated()
+        ).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception{
+        AuthenticationManagerBuilder builder=httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+
+        builder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+
+        return builder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+
+
 }
