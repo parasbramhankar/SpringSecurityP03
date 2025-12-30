3767f5d7f749ec4dafdf4cb562342ad28359bb87
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-30 11:14:32 +0530

    commit

--- src/main/java/com/example/SpringSecurityP03/Service/CustomUserDetailsService.java
+++ src/main/java/com/example/SpringSecurityP03/Service/CustomUserDetailsService.java
@@ -1,22 +1,24 @@
-package com.example.SpringSecurityP03.Service;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class CustomUserDetailsService implements UserDetailsService {
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        if(!username.equals("paras")){
-            throw  new UsernameNotFoundException("user not found");
-        }
-
-        String encodePass=new BCryptPasswordEncoder().encode("root");
-
-        return User.withUsername(username).password(encodePass).roles("USER").build();
-    }
-}
+//package com.example.SpringSecurityP03.Service;
+//
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        if(!username.equals("paras")){
+//            throw  new UsernameNotFoundException("user not found");
+//        }
+//
+//        String encodePass=new BCryptPasswordEncoder().encode("root");
+//
+//        return User.withUsername(username).password(encodePass).roles("ADMIN").build();
+//    }
+//}
